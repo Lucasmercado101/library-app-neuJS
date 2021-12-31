e3a9216b275f7b3c096419b22d0699e847300644
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-31 16:45:41 -0300

    added Elm

---
+++ resources/src/Main.elm
@@ -0,0 +1,79 @@
+module Main exposing (main)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Url
+
+
+
+-- elm make src/Main.elm --output=js/elm.js
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+        }
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    , property : String
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( Model key url "modelInitialValue", Cmd.none )
+
+
+type Msg
+    = Msg1
+    | Msg2
+    | UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Msg1 ->
+            ( model, Cmd.none )
+
+        Msg2 ->
+            ( model, Cmd.none )
+
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Title"
+    , body =
+        [ div []
+            [ text "New Application" ]
+        ]
+    }

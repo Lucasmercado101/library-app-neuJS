458a7728369f3f8be2832fe123a18265956d490c
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-31 17:26:32 -0300

    added and commented out a function

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,18 @@ Neutralino.init();
 
 Neutralino.events.on("windowClose", () => Neutralino.app.exit());
 
+// async function readFile() {
+//   let entries = await Neutralino.os.showOpenDialog("Read your JSON file", {
+//     filters: [{ name: "All files", extensions: ["txt", "json"] }]
+//   });
+//   if (!entries[0]) return;
+//   const file = entries[0];
+//   const data = await Neutralino.filesystem.readFile(file);
+//   console.log(JSON.parse(data));
+// }
+
+// readFile();
+
 Elm.Main.init({
   node: document.getElementById("main")
 });

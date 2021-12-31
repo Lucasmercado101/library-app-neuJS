5d30ec517711c06fd8740d835789fc96965ba9d8
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-31 18:11:31 -0300

    created simpleEmptyState helper fn

--- resources/src/Main.elm
+++ resources/src/Main.elm
@@ -3,6 +3,11 @@ module Main exposing (main)
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
+import Html.Events exposing (onClick)
+import Svg
+import Svg.Attributes
+import SvgIcons exposing (bookOpenOutline, plusOutline)
+import TailwindHelpers as TW exposing (..)
 import Url
 
 
@@ -65,7 +70,101 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Title"
     , body =
-        [ div []
-            [ text "New Application" ]
+        [ div
+            [ TW.apply
+                [ p_36
+                ]
+            ]
+            [ simpleEmptyState
+                { mainIcon = bookOpenOutline
+                , title = "No books"
+                , subtitle = "Get started by adding a book"
+                , buttonText = "Add book"
+                , buttonIcon = Just plusOutline
+                }
+            ]
         ]
     }
+
+
+simpleEmptyState :
+    { mainIcon : List (Svg.Attribute Msg) -> Html Msg
+    , title : String
+    , subtitle : String
+    , buttonText : String
+    , buttonIcon : Maybe (List (Svg.Attribute Msg) -> Html Msg)
+    }
+    -> Html Msg
+simpleEmptyState { mainIcon, title, subtitle, buttonText, buttonIcon } =
+    column
+        [ TW.apply
+            [ m_auto
+            , text_center
+            , gap_y_3
+            ]
+        ]
+        [ mainIcon
+            [ [ w_12, h_12, opacity_40, mx_auto ]
+                |> String.join " "
+                |> Svg.Attributes.class
+            ]
+        , column []
+            [ p
+                [ TW.apply
+                    [ text_black
+                    , font_semibold
+                    ]
+                ]
+                [ text title ]
+            , p
+                [ TW.apply
+                    [ text_black
+                    , font_semibold
+                    , opacity_50
+                    ]
+                ]
+                [ text subtitle ]
+            ]
+        , button
+            [ TW.apply
+                [ flex
+                , px_3
+                , py_2
+                , gap_x_2
+                , font_semibold
+                , block
+                , rounded
+                , mx_auto
+                , text_white
+                , shadow_md
+                , bg_blue_600
+                , hover [ bg_blue_700 ]
+                ]
+            ]
+            [ case buttonIcon of
+                Just icon ->
+                    icon
+                        [ [ w_6, h_6 ]
+                            |> String.join " "
+                            |> Svg.Attributes.class
+                        ]
+
+                Nothing ->
+                    text ""
+            , text
+                buttonText
+            ]
+        ]
+
+
+
+-- Tailwind Helpers
+
+
+column : List (Attribute msg) -> List (Html msg) -> Html msg
+column attrs content =
+    div ([ TW.apply [ flex, flex_col ] ] ++ attrs) content
+
+
+row content =
+    div [ TW.apply [ flex, flex_row ] ] content

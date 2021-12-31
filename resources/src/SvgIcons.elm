1738052273cbebe97b972ae349bfce3524f09df3
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-31 18:11:20 -0300

    added plusOutlineIcon

--- resources/src/SvgIcons.elm
+++ resources/src/SvgIcons.elm
@@ -5,6 +5,10 @@ import Svg
 import Svg.Attributes as SA
 
 
+
+--  https://heroicons.com/
+
+
 bookOpenOutline : List (Svg.Attribute msg) -> Html msg
 bookOpenOutline attrs =
     Svg.svg
@@ -19,6 +23,26 @@ bookOpenOutline attrs =
             , SA.strokeLinejoin "round"
             , SA.strokeWidth "2"
             , SA.d "M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+            ]
+            []
+        ]
+
+
+plusOutline : List (Svg.Attribute msg) -> Html msg
+plusOutline attrs =
+    Svg.svg
+        ([ SA.class "h-6 w-6"
+         , SA.fill "none"
+         , SA.viewBox "0 0 24 24"
+         , SA.stroke "currentColor"
+         ]
+            ++ attrs
+        )
+        [ Svg.path
+            [ SA.strokeLinecap "round"
+            , SA.strokeLinejoin "round"
+            , SA.strokeWidth "2"
+            , SA.d "M12 4v16m8-8H4"
             ]
             []
         ]

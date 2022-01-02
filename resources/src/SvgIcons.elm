fc1548d9b3fc0cfd84cc05150aa7d06d09796d97
Lucasmercado101 <lucasmercado101@gmail.com>
2022-01-02 20:19:09 -0300

    added photographOutline

--- resources/src/SvgIcons.elm
+++ resources/src/SvgIcons.elm
@@ -78,3 +78,22 @@ xOutline attrs =
             ]
             []
         ]
+
+
+photographOutline : List (Svg.Attribute msg) -> Html msg
+photographOutline attrs =
+    Svg.svg
+        ([ SA.fill "none"
+         , SA.viewBox "0 0 24 24"
+         , SA.stroke "currentColor"
+         ]
+            ++ attrs
+        )
+        [ Svg.path
+            [ SA.strokeLinecap "round"
+            , SA.strokeLinejoin "round"
+            , SA.strokeWidth "2"
+            , SA.d "M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+            ]
+            []
+        ]
